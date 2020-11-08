6db85d9b6a1e4e8f743190ac29b11e8f622d912e
robertochacona <hola.hack09@gmail.com>
2020-11-08 01:43:25 -0400

    new changes 3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-let logo = document.getElementById('imagen');
+// let logo = document.getElementById('imagen');
 
-logo.addEventListener('click', function() {
-    logo.classList.remove('fadeIn');
-    logo.classList.add('fadeIn');
+// logo.addEventListener('click', function() {
+//     logo.classList.remove('fadeIn');
+//     logo.classList.add('fadeIn');
 
-    if (confirm('Te gusta jugar?')) {
-        var audio = document.getElementById("audio");
-        audio.play();
-    } else {
-        alert('Testing good...')
-    }
-
-});
+// });
